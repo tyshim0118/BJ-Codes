7d01dd07ad7eb97358ea943f2c174152bacf3e26
Logicism <121869310+tyshim0118@users.noreply.github.com>
2025-05-11 14:23:42 +0900

    Create BJ11718.cpp
    BaekJoon 11718 Codes.

---
+++ BJ11718.cpp
@@ -0,0 +1,11 @@
+//그대로 출력하기 - 브론즈 3
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string A;
+    while (getline(cin,A)) {
+        cout << A << endl;
+    }
+}
