fcf5c835efe06a49d4b354edcda3af87facfd753
Logicism <121869310+tyshim0118@users.noreply.github.com>
2025-05-17 14:38:10 +0900

    Create BJ2566.cpp
    BaekJoon 2566 Codes.

---
+++ BJ2566.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int array[9][9], max = 0, mr = 1, mc = 1;
+    for (int i = 0; i < 9 ; i++) {
+        for (int j = 0; j < 9 ; j++) {
+            cin >> array[i][j];
+            if (array[i][j] > max) {
+                max = array[i][j];
+                mr = i+1;
+                mc = j+1;
+            }
+        }
+    }
+    cout << max << endl << mr << " " << mc;
+}
