b0d1c3d6d04c030889293a9a29f8b57ddaf4f829
Logicism <121869310+tyshim0118@users.noreply.github.com>
2025-05-21 21:41:19 +0900

    Create BJ2292.cpp
    BaekJoon 2292 Codes.

---
+++ BJ2292.cpp
@@ -0,0 +1,21 @@
+//벌집 - 브론즈 2
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 0, X, deno = 0, nume = 1, oddSwitch = 1;
+    cin >> X;
+    while (i < X) {
+        deno++;
+        i += deno;
+    }
+    if (!(deno%2)) oddSwitch = 0;
+    while (i > X) {
+        i--;
+        deno--;
+        nume++;
+    }
+    if(oddSwitch) cout << nume << "/" << deno;
+    else cout << deno << "/" << nume;
+}
