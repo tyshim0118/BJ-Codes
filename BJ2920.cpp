4554b76ab5731659dfc4b5a9ec058099944833c9
Logicism <121869310+tyshim0118@users.noreply.github.com>
2025-05-26 11:54:24 +0900

    Update BJ2920.cpp
    BaekJoon 2920 Codes.

--- BJ2920.cpp
+++ BJ2920.cpp
@@ -4,24 +4,12 @@
 using namespace std;
 
 int main() {
-    int T, Os, result;
-    string ox;
-    cin >> T;
-    for (int i = 0; i < T; i++) {
-        cin >> ox;
-        string::iterator it = ox.begin();
-        Os = 0;
-        result = 0;
-        while (*it) {
-            if (*it == 'O') {
-                Os++;
-                result += Os;
-                it++;
-            } else {
-                Os = 0;
-                it++;
-            }
-        }
-        cout << result << '\n';
-    }
+    string s;
+    getline(cin, s);
+    if (s == "1 2 3 4 5 6 7 8")
+        cout << "ascending";
+    else if (s == "8 7 6 5 4 3 2 1")
+        cout << "descending";
+    else
+        cout << "mixed";
 }
