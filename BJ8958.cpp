efe8b998a4c29fb478558c837d6afd2280381b6b
Logicism <121869310+tyshim0118@users.noreply.github.com>
2025-05-26 11:53:16 +0900

    Create BJ8958.cpp
    BaekJoon 8958 Codes.

---
+++ BJ8958.cpp
@@ -0,0 +1,26 @@
+//OX퀴즈 - 브론즈 2
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T, Os, result;
+    string ox;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        cin >> ox;
+        string::iterator it = ox.begin();
+        Os = 0;
+        result = 0;
+        while (*it) {
+            if (*it == 'O') {
+                Os++;
+                result += Os;
+                it++;
+            } else {
+                Os = 0;
+                it++;
+            }
+        }
+        cout << result << '\n';
+    }
+}
