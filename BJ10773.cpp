0c55057b7ce28b437286335f629140649dc21457
Logicism <121869310+tyshim0118@users.noreply.github.com>
2025-06-26 18:53:59 +0900

    Create BJ10773.cpp
    BaekJoon 10773 Codes.

---
+++ BJ10773.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    int K, res = 0;
+    cin >> K;
+    stack<int> zero;
+    for (int i = 0; i < K; i++) {
+        int n;
+        cin >> n;
+        if (n)
+            zero.push(n);
+        else
+            zero.pop();
+    }
+    while (!zero.empty()) {
+        res += zero.top();
+        zero.pop();
+    }
+    cout << res;
+}
