6998553cb3e291ab74ba181bc92679cebb4c12e3
Logicism <121869310+tyshim0118@users.noreply.github.com>
2025-06-28 11:01:21 +0900

    Create BJ14626.cpp
    BaekJoon 14626 Codes.

---
+++ BJ14626.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string ISBN;
+    cin >> ISBN;
+    int dmgI, sum = 0;
+    for (int i = 0; i < 13; i++) {
+        if (ISBN[i] == '*') {
+            dmgI = i;
+            continue;
+        }
+        if (i % 2)
+            sum += 3 * (ISBN[i] - '0');
+        else
+            sum += ISBN[i] - '0';
+    }
+    int dmgN = 0;
+    if (dmgI % 2) {
+        while ((sum + 3 * dmgN) % 10) dmgN++;
+    } else {
+        while ((sum + dmgN) % 10) dmgN++;
+    }
+    cout << dmgN << "\n";
+}
