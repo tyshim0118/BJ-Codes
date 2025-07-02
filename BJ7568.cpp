135295e911abb1dbc778dbcd9b2b70b57cf1b315
Logicism <121869310+tyshim0118@users.noreply.github.com>
2025-07-02 17:45:37 +0900

    Create BJ7568.cpp
    BaekJOON 7568 Codes.

---
+++ BJ7568.cpp
@@ -0,0 +1,38 @@
+//덩치 - 실버 5
+
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+struct Dungchi {
+    int weight = 0, height = 0, rank = 1;
+		bool operator>(Dungchi);
+    bool operator<(Dungchi);
+};
+bool Dungchi::operator>(Dungchi d) { 
+    return (weight > d.weight) && (height > d.height);
+}
+bool Dungchi::operator<(Dungchi d) { 
+    return (weight < d.weight) && (height < d.height); 
+}
+
+int main() {
+    int N;
+    cin >> N;
+    vector<Dungchi> d;
+    for (int i = 0; i < N; i++) {
+        int w, h;
+        cin >> w >> h;
+        Dungchi temp = {w, h};
+        auto it = d.begin();
+        while (it != d.end()) {
+            if (*it < temp) (*it).rank++;
+            else if (*it > temp) temp.rank++;
+            it++;
+        }
+        d.push_back(temp);
+    }
+    for(int i = 0 ; i < N; i++) {
+        cout << d[i].rank << " ";
+    }
+}
