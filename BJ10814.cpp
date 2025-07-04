3a27d107b9191ee5aa5b03a784793c9307b4a46b
Logicism <121869310+tyshim0118@users.noreply.github.com>
2025-07-04 09:08:32 +0900

    Create BJ10814.cpp
    BaekJOOn 10814 Codes.

---
+++ BJ10814.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+struct member {
+    int age, order;
+    string name;
+};
+
+bool cmp(member p1, member p2) {
+    if (p1.age == p2.age)
+        return p1.order < p2.order;
+    else
+        return p1.age < p2.age;
+}
+
+int main() {
+    int N, age;
+    string name;
+    cin >> N;
+    vector<member> v;
+    for (int i = 0; i < N; i++) {
+        cin >> age >> name;
+        v.push_back({age, i, name});
+    }
+    sort(v.begin(), v.end(), cmp);
+    for (member m : v) cout << m.age << ' ' << m.name << '\n';
+}
